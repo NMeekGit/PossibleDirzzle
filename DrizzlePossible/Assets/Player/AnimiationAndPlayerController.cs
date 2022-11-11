6ca52dcf28c4952fd29bdc094d2775f9be564105
noah <noah@gitlab.cs470.local>
2022-11-10 18:22:05 -0800

    Finished: Player Input & Animation

--- DrizzlePossible/Assets/Player/AnimiationAndPlayerController.cs
+++ DrizzlePossible/Assets/Player/AnimiationAndPlayerController.cs
@@ -7,20 +7,38 @@ public class AnimiationAndPlayerController : MonoBehaviour
 {
     PlayerInput playerInput;
     CharacterController characterController;
+    Animator animator;
+
+    int isWalkingHash;
+    int isRunningHash;
 
     Vector2 currentMovementInput;
     Vector3 currentMovement;
+    Vector3 currentRunMovement;
     bool isMovementPressed;
-    public float rotationFactorPerFrame;
+    bool isRunPressed;
+    public float rotationFactorPerFrame = 15.0f;
+    public float runMultiplier = 3.0f;
 
     void Awake()
     {
         playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+        isRunningHash = Animator.StringToHash("isRunning");
 
         playerInput.PlayerController.Move.started += OnMovementInput;
         playerInput.PlayerController.Move.canceled += OnMovementInput;
         playerInput.PlayerController.Move.performed += OnMovementInput;
+        playerInput.PlayerController.Run.started += OnRun;
+        playerInput.PlayerController.Run.canceled += OnRun;
+    }
+
+    void OnRun(InputAction.CallbackContext context)
+    {
+        isRunPressed = context.ReadValueAsButton();
     }
 
     void HandleRotation()
@@ -44,12 +62,57 @@ public class AnimiationAndPlayerController : MonoBehaviour
         currentMovementInput = context.ReadValue<Vector2>();
         currentMovement.x = currentMovementInput.x;
         currentMovement.z = currentMovementInput.y;
+        currentRunMovement.x = currentMovementInput.x * runMultiplier;
+        currentRunMovement.z = currentMovementInput.y * runMultiplier;
         isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+    }
+
+    void HandleAnimation()
+    {
+        bool isWalking = animator.GetBool(isWalkingHash);
+        bool isRunning = animator.GetBool(isRunningHash);
+
+        if (isMovementPressed && !isWalking) {
+            animator.SetBool(isWalkingHash, true);
+        }
+        else if (!isMovementPressed && isWalking) {
+            animator.SetBool(isWalkingHash, false);
+        }
+
+        if ((isMovementPressed && isRunPressed) && !isRunning) {
+            animator.SetBool(isRunningHash, true);
+        }
+        else if ((!isMovementPressed || !isRunPressed) && isRunning) {
+            animator.SetBool(isRunningHash, false);
+        }
+    }
+
+    void HandleGravity()
+    {
+        if (characterController.isGrounded) {
+            float groundedGravity = -0.05f;
+            currentMovement.y = groundedGravity;
+            currentRunMovement.y = groundedGravity;
+        }
+        else {
+            float gravity = -9.8f;
+            currentMovement.y += gravity;
+            currentRunMovement.y += gravity;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        HandleRotation();
+        HandleAnimation();
+
+        if (isRunPressed) {
+            characterController.Move(currentRunMovement * Time.deltaTime);
+        }
+        else {
+            characterController.Move(currentMovement * Time.deltaTime);
+        }
         characterController.Move(currentMovement * Time.deltaTime);
     }
     
